a148072c436fc826f18574454d43a999e51b7f20
Bratah123 <brandonnguyen3301@gmail.com>
2022-12-06 18:04:38 -0800

    FEAT: Center align the game screen

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,11 +2,12 @@ import Phaser from 'phaser'
 
 import MainMenu from './scenes/main_menu'
 
-const config = {
+const game = {
 	type: Phaser.AUTO,
-	width: 1280,
+	width: 1280, // TODO: Figure out how big this game should be
 	height: 720,
+	parent: 'moogiwara',
 	scene: [MainMenu]
 }
 
-export default new Phaser.Game(config)
+export default new Phaser.Game(game)
