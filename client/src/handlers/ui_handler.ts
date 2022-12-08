818586bd150bce5375db5163159651476ce0f003
Bratah123 <brandonnguyen3301@gmail.com>
2022-12-07 17:06:09 -0800

    FEAT: Separated UI responsibility into own handler

---
+++ client/src/handlers/ui_handler.ts
@@ -0,0 +1,50 @@
+export default class UiHandler {
+  scene: Phaser.Scene;
+  constructor(scene: Phaser.Scene) {
+    this.scene = scene
+  }
+
+  initUi = () => {
+    // Create a button to go back to the main menu
+    const backButton = this.scene.add.text(0, 0, 'Back');
+    backButton.setStyle({ fontSize: '16px', fill: '#000000' });
+
+    backButton.setInteractive();
+    backButton.on('pointerdown', () => {
+        this.scene.scene.start('main-menu');
+    });
+
+    // Makes the button turn red when hovered over
+    backButton.on('pointerover', () => {
+        backButton.setStyle({ fill: '#ff0000' });
+    });
+
+    // Turn the button back to normal color after the mouse leaves
+    backButton.on('pointerout', () => {
+        backButton.setStyle({ fill: '#000000' });
+    });
+
+    // Create a button to go back to end turn in the middle of the screen
+    // TODO: Add an actual button image
+    const endTurn = this.scene.add.text(1000, 360, 'End Turn');
+    endTurn.setStyle({ fontSize: '16px', fill: '#000000' });
+
+    endTurn.setInteractive();
+    endTurn.on('pointerdown', this.onEndTurn);
+
+    // Makes the button turn red when hovered over
+    endTurn.on('pointerover', () => {
+        endTurn.setStyle({ fill: '#ff0000' });
+    });
+
+    // Turn the button back to normal color after the mouse leaves
+    endTurn.on('pointerout', () => {
+        endTurn.setStyle({ fill: '#000000' });
+    });
+  }
+
+
+  onEndTurn = () => {
+    console.log('End this player\'s turn');
+  }
+}
--- client/src/scenes/game_board.ts
+++ client/src/scenes/game_board.ts
@@ -1,8 +1,17 @@
 import Phaser from 'phaser';
+import Player from '../game/player';
+import GameHandler from '../handlers/game_handler';
+import UiHandler from '../handlers/ui_handler';
 
 export default class GameBoard extends Phaser.Scene {
+
+  gameHandler: GameHandler;
+  uiHandler: UiHandler;
+
   constructor() {
     super('game-board');
+    this.gameHandler = new GameHandler(this, new Player(), new Player());
+    this.uiHandler = new UiHandler(this);
   }
 
   preload() {
@@ -11,51 +20,13 @@ export default class GameBoard extends Phaser.Scene {
 
   create() {
     this.add.image(0, 0, 'background').setOrigin(0, 0);
-
-    // Create a button to go back to the main menu
-    const backButton = this.add.text(0, 0, 'Back');
-    backButton.setStyle({ fontSize: '16px', fill: '#000000' });
-
-    backButton.setInteractive();
-    backButton.on('pointerdown', () => {
-      this.scene.start('main-menu');
-    });
-
-    // Makes the button turn red when hovered over
-    backButton.on('pointerover', () => {
-      backButton.setStyle({ fill: '#ff0000' });
-    });
-
-    // Turn the button back to normal color after the mouse leaves
-    backButton.on('pointerout', () => {
-      backButton.setStyle({ fill: '#000000' });
-    });
-
-    // Create a button to go back to end turn in the middle of the screen
-    // TODO: Add an actual button image
-    const endTurn = this.add.text(1000, 360, 'End Turn');
-    endTurn.setStyle({ fontSize: '16px', fill: '#000000' });
-
-    endTurn.setInteractive();
-    endTurn.on('pointerdown', this.onEndTurn);
-
-    // Makes the button turn red when hovered over
-    endTurn.on('pointerover', () => {
-      endTurn.setStyle({ fill: '#ff0000' });
-    });
-
-    // Turn the button back to normal color after the mouse leaves
-    endTurn.on('pointerout', () => {
-      endTurn.setStyle({ fill: '#000000' });
-    });
+    // Initialize any UI Here
+    this.uiHandler.initUi();
+    // Start game here
+    this.gameHandler.startGame();
   }
 
   // update(time: number, delta: number): void {
 
   // }
-
-  // Called when the player clicks end turn button
-  onEndTurn = () => {
-    console.log("End this player's turn");
-  };
 }
