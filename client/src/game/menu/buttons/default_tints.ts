2d6a260b89a47a3d209cf7e3908062bfc0ae0c5b
KOOKIIEStudios <25145447+KOOKIIEStudios@users.noreply.github.com>
2022-12-10 01:09:22 +0800

    FIX: Missing button helper modules

---
+++ client/src/game/menu/buttons/default_tints.ts
@@ -0,0 +1,4 @@
+export const defaultActiveTintColor = 0xD4D4D4;
+export const defaultHoverTintColor = 0xF0F0F0;
+export const defaultHollowActiveTintColor = 0x444444;
+export const defaultHollowHoverTintColor = 0xE7E7E7;
