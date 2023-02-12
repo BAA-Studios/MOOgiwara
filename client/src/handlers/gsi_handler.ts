77f765aaf3620c997a318b81f802fa1ce9a530fd
KOOKIIEStudios <25145447+KOOKIIEStudios@users.noreply.github.com>
2023-02-12 12:39:32 +0800

    FIX: Missing queue packet

---
+++ client/src/handlers/gsi_handler.ts
@@ -0,0 +1,9 @@
+/**
+ * Extracts the user's email address from a given JWT token
+ * @param jwtToken JWT token received via GSI
+ * @returns {string} email address encoded inside the JWT token
+ */
+export function extractEmailFromToken(jwtToken): string {
+    // TODO: find out the type for token
+}
+
