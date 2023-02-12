0760dd580444422802b13d76d569c28e0f9acff9
KOOKIIEStudios <25145447+KOOKIIEStudios@users.noreply.github.com>
2023-02-12 12:49:39 +0800

    FEAT (client): Skeleton for JWT token handling

--- client/src/handlers/gsi_handler.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Extracts the user's email address from a given JWT token
- * @param jwtToken JWT token received via GSI
- * @returns {string} email address encoded inside the JWT token
- */
-export function extractEmailFromToken(jwtToken): string {
-    // TODO: find out the type for token
-}
-
